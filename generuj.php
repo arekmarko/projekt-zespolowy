11b56082d317e859d8acbca3045ac44fe79be244
Arek <arekmarko8@gmail.com>
2021-03-26 15:39:20 +0100

    Poprawiono navbar

--- generuj.php
+++ generuj.php
@@ -5,46 +5,73 @@
         <link rel="stylesheet" href="styles.css">
     </head>
     <body>
-        <ul class="navbar">
-            <li id=home><a href="index.php">PSK</a></li>
-            <li class="dropdown">
-                <a href="javascript:void(0)" class="dropbtn">Znajdź grupę</a>
-                <div class="dropdown-content">
+    <ul class="navbar">
+        <li id=home><a href="#">PSK</a></li>
+        <li class="dropdown">
+            <a href="javascript:void(0)" class="dropbtn">Znajdź grupę</a>
+            <div class="dropdown-content">
+                <div class="dropdown2">
+                <a href="#">1 rok
+                    <i class="arrow right"></i></a>
+                <div class="dropdown2-content">
                     <a href="#">1ID11A</a>
                     <a href="#">1ID11B</a>
+                    <a href="#">1ID12A</a>
+                    <a href="#">1ID12B</a>
+                </div>
+                </div>
+                <div class="dropdown2">
+                <a href="#">2 rok
+                    <i class="arrow right"></i></a>
+                <div class="dropdown2-content">
                     <a href="#">2ID11A</a>
                     <a href="#">2ID11B</a>
+                    <a href="#">2ID12A</a>
+                    <a href="#">2ID12B</a>
+                </div>
+                </div>
+                <div class="dropdown2">
+                <a href="#">3 rok
+                    <i class="arrow right"></i></a>
+                <div class="dropdown2-content">
                     <a href="#">3ID11A</a>
                     <a href="#">3ID11B</a>
+                    <a href="#">3ID12A</a>
+                    <a href="#">3ID12B</a>
+                    <a href="#">3ID13A</a>
+                    <a href="#">3ID13B</a>
                 </div>
-            </li>
-            <li class="dropdown">
-                <a href="#" class="dropbtn">Znajdź wykładowcę</a>
-                <div class="dropdown-content">
-                    <a href="#">dr inż. Aneta Bugajska</a>
-                    <a href="#">dr inż. Arkadiusz Chrobot</a>
-                    <a href="#">dr inż. Grzegorz Łukawski</a>
-                    <a href="#">mgr Nina Kacperczyk</a>
-                    <a href="#">mgr inż. Daniel Kaczmarski</a>
-                    <a href="#">dr inż. Adam Krechowicz</a>
-                    <a href="#">dr inż. Andrzej Kułakowski</a>
-                    <a href="#">mgr inż. Paweł Pięta</a>
-                    <a href="#">dr inż. Jarosław Wikarek</a>
                 </div>
-            </li>
-            <li class="dropdown">
-                <a href="#" class="dropbtn">Znajdź przedmiot</a>
-                <div class="dropdown-content">
-                    <a href="#">Inżynieria programowania</a>
-                    <a href="#">Język obcy 4</a>
-                    <a href="#">Grafika komputerowa</a>
-                    <a href="#">Metody obliczeniowe</a>
-                    <a href="#">Programowanie współbieżne</a>
-                    <a href="#">Projektowanie aplikacji internetowych 1</a>
                 </div>
-            </li>
-            <li class="zalbtn" style="float:right";><a href="login.php">Wyloguj się</a></li>
-        </ul>
+            </div>
+        </li>
+        <li class="dropdown">
+            <a href="#" class="dropbtn">Znajdź wykładowcę</a>
+            <div class="dropdown-content">
+                <a href="#">dr inż. Aneta Bugajska</a>
+                <a href="#">dr inż. Arkadiusz Chrobot</a>
+                <a href="#">dr inż. Grzegorz Łukawski</a>
+                <a href="#">mgr Nina Kacperczyk</a>
+                <a href="#">mgr inż. Daniel Kaczmarski</a>
+                <a href="#">dr inż. Adam Krechowicz</a>
+                <a href="#">dr inż. Andrzej Kułakowski</a>
+                <a href="#">mgr inż. Paweł Pięta</a>
+                <a href="#">dr inż. Jarosław Wikarek</a>
+            </div>
+        </li>
+        <li class="dropdown">
+            <a href="#" class="dropbtn">Znajdź przedmiot</a>
+            <div class="dropdown-content">
+                <a href="#">Inżynieria programowania</a>
+                <a href="#">Język obcy 4</a>
+                <a href="#">Grafika komputerowa</a>
+                <a href="#">Metody obliczeniowe</a>
+                <a href="#">Programowanie współbieżne</a>
+                <a href="#">Projektowanie aplikacji internetowych 1</a>
+            </div>
+        </li>
+        <li class="zalbtn" style="float:right";><a href="login.php">Wyloguj się</a></li>
+    </ul>
         <div class="generator">
             <div class="genbts">
                 <button class="tablinks" onclick="openG(event, 'Sala')">Dodaj salę</button>
